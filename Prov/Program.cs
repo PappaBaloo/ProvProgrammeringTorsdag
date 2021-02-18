45d61c14fa48fa3487c97230ee5682c690751c12
Charlie Sesser Nylund <charlie.sessernylund@elev.ga.ntig.se>
2021-02-18 13:36:33 +0100

    first commit

---
+++ Prov/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
